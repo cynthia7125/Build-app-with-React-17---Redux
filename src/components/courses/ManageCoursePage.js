8c5751124b5dfff02b6692725043a4b230255044
Reyhana Cherop <reyhanacynthia@gmail.com>
2023-09-09 00:34:36 +0300

    Update course list

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,52 +1,85 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { loadCourses } from "../../redux/actions/courseActions";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
+import { useHistory } from "react-router-dom"; 
 
-function ManageCoursesPage({courses, authors, loadAuthors, loadCourses}) {
-    useEffect(() => {
-    
-        if (courses.length === 0) {
-            loadCourses().catch((error) => {
-                alert("Loading courses failed" + error);
-            });
-        }
-        
-        if (authors.length === 0) {
-            loadAuthors().catch((error) => {
-                alert("Loading authors failed" + error);
-            });
-        }
+function ManageCoursesPage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  saveCourse,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({ ...props.course });
+  const history = useHistory();
 
-}, [])
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch((error) => {
+        alert("Loading courses failed" + error);
+      });
+    }
 
-    return (
-      <>
-        <h2>Manage Courses</h2>
-      </>
-    );
+    if (authors.length === 0) {
+      loadAuthors().catch((error) => {
+        alert("Loading authors failed" + error);
+      });
+    }
+  }, []);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse((prevCourse) => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value,
+    }));
   }
 
+  function handleSave(event) {
+    event.preventDefault();
+    saveCourse(course).then(() => {
+      history.push("/courses");
+    });
+  }
+
+  return (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+      onSave={handleSave}
+    />
+  );
+}
 
 ManageCoursesPage.propTypes = {
+  course: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   loadAuthors: PropTypes.func.isRequired,
   loadCourses: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
+    course: newCourse,
     courses: state.courses,
     authors: state.authors,
   };
 }
 
 const mapDispatchToProps = {
-
-      loadCourses,
-      loadAuthors,
-}
+  loadCourses,
+  loadAuthors,
+  saveCourse,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursesPage);
