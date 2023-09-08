709601a76b00c4be6032429e3634398d76bf67b5
Reyhana Cherop <reyhanacynthia@gmail.com>
2023-09-09 00:38:22 +0300

    Open a course populates and persists after reload

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -5,7 +5,6 @@ import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import CourseForm from "./CourseForm";
 import { newCourse } from "../../../tools/mockData";
-import { useHistory } from "react-router-dom"; 
 
 function ManageCoursesPage({
   courses,
@@ -13,17 +12,19 @@ function ManageCoursesPage({
   loadAuthors,
   loadCourses,
   saveCourse,
+  history,
   ...props
 }) {
   const [course, setCourse] = useState({ ...props.course });
-  const [errors, setErrors] = useState({ ...props.course });
-  const history = useHistory();
+  const [errors, setErrors] = useState();
 
   useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((error) => {
         alert("Loading courses failed" + error);
       });
+    } else {
+      setCourse({ ...props.course });
     }
 
     if (authors.length === 0) {
@@ -31,7 +32,7 @@ function ManageCoursesPage({
         alert("Loading authors failed" + error);
       });
     }
-  }, []);
+  }, [props.course]);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -66,11 +67,21 @@ ManageCoursesPage.propTypes = {
   loadAuthors: PropTypes.func.isRequired,
   loadCourses: PropTypes.func.isRequired,
   saveCourse: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired
 };
 
-function mapStateToProps(state) {
+export function getCourseBySlug(courses, slug) {
+  return courses.find((course) => course.slug === slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+    const course =
+      slug && state.courses.length > 0
+        ? getCourseBySlug(state.courses, slug)
+        : newCourse;
   return {
-    course: newCourse,
+    course,
     courses: state.courses,
     authors: state.authors,
   };
