d8f3c069677e5182534033a6393070eaa9f764f4
Reyhana Cherop <reyhanacynthia@gmail.com>
2023-09-08 20:49:14 +0300

    Action types constants

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 
 class CoursesPage extends React.Component {
   constructor(props) {
@@ -17,12 +21,13 @@ class CoursesPage extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    // this.props.actions.createCourse(this.state.course);
+    this.props.actions.createCourse(this.state.course);
   }
 
   render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
+      return (
+        // we use bind(this) cause the methods are not arrow methods
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <h2>Courses</h2>
         <h3>Add Course</h3>
         <input
@@ -31,13 +36,31 @@ class CoursesPage extends React.Component {
           value={this.state.course.title}
         />
 
-        <input type="submit" value="Save" />
-        {/* {this.props.courses.map((course) => (
+              <input type="submit" value="Save" />
+              {/* list all courses */}
+        {this.props.courses.map((course) => (
           <div key={course.title}>{course.title}</div>
-        ))} */}
+        ))}
       </form>
     );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
